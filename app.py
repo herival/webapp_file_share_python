7ed2b301f47e2043e980c4039aa86ed87f4e1643
heri <herimalala.val@gmail.com>
2024-08-20 16:12:13 +0200

    build

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+# import logging
+from waitress import serve
 import os
+import socket
 
 app = Flask(__name__)
+
+# Codes de couleurs ANSI
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"
+
+# Désactivez l'avertissement en modifiant le niveau de journalisation de Flask
+# log = logging.getLogger('werkzeug')
+# log.setLevel(logging.ERROR)
 
 # Définir le dossier pour stocker les fichiers téléchargés
 UPLOAD_FOLDER = 'uploads'
@@ -40,4 +53,28 @@ def download_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    host = '0.0.0.0'
+    port = 5000
+    message = f"""{YELLOW}
+
+██╗  ██╗███████╗██████╗ ██╗    ███████╗ ██████╗ ███████╗████████╗
+██║  ██║██╔════╝██╔══██╗██║    ██╔════╝██╔═══██╗██╔════╝╚══██╔══╝
+███████║█████╗  ██████╔╝██║    ███████╗██║   ██║█████╗     ██║   
+██╔══██║██╔══╝  ██╔══██╗██║    ╚════██║██║   ██║██╔══╝     ██║   
+██║  ██║███████╗██║  ██║██║    ███████║╚██████╔╝██║        ██║   
+╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝    ╚══════╝ ╚═════╝ ╚═╝        ╚═╝   
+***********Développée par Herimalala VALISOA*********************
+    {RESET}                                                           
+    """
+    # Obtenir l'adresse IP locale
+    local_ip = socket.gethostbyname(socket.gethostname())
+    
+    # Affiche un message dans le terminal avant de lancer l'application
+    print(message)
+    print(f"L'application Flask est accessible sur {GREEN}http://{local_ip}:{port}")
+    print(f"{RED}Appuyer sur ctrl+c pour quitter...{RESET}")
+
+    serve(app, host=host, port=port)
+    
+    # serveur pour developpement
+    # app.run(debug=False, host='0.0.0.0', port=5000)
--- app_launcher.spec
+++ app_launcher.spec
@@ -1,12 +1,11 @@
 # -*- mode: python ; coding: utf-8 -*-
 
-
 a = Analysis(
-    ['app_launcher.py'],
+    ['app.py'],
     pathex=[],
     binaries=[],
-    datas=[],
-    hiddenimports=[],
+    datas=[('templates', 'templates'), ('static', 'static')],
+    hiddenimports=['flask', 'waitress', 'os', 'socket'],
     hookspath=[],
     hooksconfig={},
     runtime_hooks=[],
@@ -14,25 +13,32 @@ a = Analysis(
     noarchive=False,
     optimize=0,
 )
-pyz = PYZ(a.pure)
+pyz = PYZ(a.pure, a.zipped_data)
 
 exe = EXE(
     pyz,
     a.scripts,
-    a.binaries,
-    a.datas,
     [],
-    name='app_launcher',
+    exclude_binaries=True,
+    name='hws',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
     upx=True,
-    upx_exclude=[],
-    runtime_tmpdir=None,
     console=True,
     disable_windowed_traceback=False,
     argv_emulation=False,
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
+)
+coll = COLLECT(
+    exe,
+    a.zipfiles,
+    a.binaries,
+    a.datas,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    name='heriwebshare',
 )
